71d19e51afbc7531f12ad9f1d359262d83539fb4
Maxcode123 <nikiforos@live.co.uk>
2022-01-06 12:56:47 +0200

    sorthelpers header file and source file

---
+++ sorthelpers.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#include "sorthelpers.h"
+
+
+int sorthelpers_less(int first_value, int second_value)
+{
+    return first_value < second_value;
+}
+
+void sorthelpers_exchange(int *array, int first_index, int second_index)
+{
+    int temp = array[first_index];
+    array[first_index] = array[second_index];
+    array[second_index] = temp;
+    return;
+}
+
+void sorthelpers_show(int *array, int length)
+{
+    for (int i=0; i<length; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return;
+}
+
+int sorthelpers_is_sorted(int *array, int length)
+{
+    for (int i=1; i<length; i++)
+    {
+        if (sorthelpers_less(array[i], array[i-1]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void sorthelpers_merge(int *array, int *aux, int lo, int mid, int hi)
+{
+    int i = lo;
+    int j = mid + 1;
+    for (int k = lo; k <= hi; k++)
+    {
+        aux[k] = array[k];
+    }
+    for (int k = lo; k <= hi; k++)
+    {
+        if (i > mid)
+        {
+            array[k] = aux[j++];
+        }
+        else if (j > hi)
+        {
+            array[k] = aux[i++];
+        }
+        else if (sorthelpers_less(aux[j], aux[i]))
+        {
+            array[k] = aux[j++];
+        }
+        else
+        {
+            array[k] = aux[i++];
+        }
+    }
+}
+
+int sorthelpers_min(int first_value, int second_value)
+{
+    return second_value ^ ((first_value ^ second_value) & -(first_value < second_value));
+}
---
+++ sorthelpers.h
@@ -0,0 +1,40 @@
+#ifndef SORTHELPERS_H
+#define SORTHELPERS_H
+
+/**
+ * Returns 1 if first value is less than second_value, else 0.
+ *
+ */
+int sorthelpers_less(int first_value, int second_value);
+
+/**
+ * Exchanges the values between the two indices of the array.
+ * 
+ */
+void sorthelpers_exchange(int *array, int first_index, int second_index);
+
+/**
+ * Prints inline the given array.
+ * 
+ */
+void sorthelpers_show(int *array, int length);
+
+/**
+ * Returns 1 if array is sorted else 0.
+ * 
+ */
+int sorthelpers_is_sorted(int *array, int length);
+
+/**
+ * Merges sorted sub-arrays array[lo..mid] with array[mid+1..hi].
+ * 
+ */
+void sorthelpers_merge(int *array, int *aux, int lo, int mid, int hi);
+
+/**
+ * Returns the minimum value between the two integer inputs.
+ * 
+ */
+int sorthelpers_min(int first_value, int second_value);
+
+#endif
